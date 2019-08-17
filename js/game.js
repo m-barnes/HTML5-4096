ef6847e6d8a57e89431a3edff9b941e9943a7e72
Michael Barnes <m.brandon.barnes@gmail.com>
2019-08-17 18:12:10 -0500

    022 Adding interface

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ var gameOptions = {
     tileSize: 200,
     tileSpacing: 20,
     boardSize: {
-        rows: 4,
-        cols: 4
+        rows: 5,
+        cols: 5
     },
     tweenSpeed: 50,
     swipeMaxTime: 1000,
@@ -41,6 +41,12 @@ class bootGame extends Phaser.Scene{
         super("BootGame");
     }
     preload(){
+        this.load.image("restart", "assets/sprites/restart.png");
+        this.load.image("scorepanel", "assets/sprites/scorepanel.png");
+        this.load.image("scorelabels", "assets/sprites/scorelabels.png");
+        this.load.image("logo", "assets/sprites/logo.png");
+        this.load.image("howtoplay", "assets/sprites/howtoplay.png");
+        this.load.image("gametitle", "assets/sprites/gametitle.png");
         this.load.image("emptytile", "assets/sprites/emptytile.png");
         this.load.spritesheet("tiles", "assets/sprites/tiles.png", {
             frameWidth: gameOptions.tileSize,
@@ -60,6 +66,17 @@ class playGame extends Phaser.Scene{
     }
     //creates board. Add sprites to board and hide them.
     create(){
+      var restartXY = this.getTilePosition(-0.9, gameOptions.boardSize.cols - 1);
+      var restartButton = this.add.sprite(restartXY.x, restartXY.y, "restart");
+      var scoreXY = this.getTilePosition(-0.9, 1);
+      this.add.image(scoreXY.x, scoreXY.y, "scorepanel");
+      this.add.image(scoreXY.x, scoreXY.y - 70, "scorelabels");
+      var gameTitle = this.add.image(10, 5, "gametitle");
+      gameTitle.setOrigin(0, -0.2);
+      var howTo = this.add.image(game.config.width, 5, "howtoplay");
+      howTo.setOrigin(1, -0.3);
+      var logo = this.add.sprite(game.config.width / 2, game.config.height, "logo");
+      logo.setOrigin(0.5, 1.2)
         this.canMove = false;
         this.boardArray = [];
         for(var i = 0; i < gameOptions.boardSize.rows; i++){
@@ -121,8 +138,9 @@ class playGame extends Phaser.Scene{
     getTilePosition(row, col){
         var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
         var posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
+
         var boardHeight = gameOptions.boardSize.rows * gameOptions.tileSize;
-        boardHeight += (gameOptions.boardSize.rows +1) * gameOptions.tileSpacing;
+        boardHeight += (gameOptions.boardSize.rows + 1) * gameOptions.tileSpacing;
         var offsetY = (game.config.height - boardHeight) / 2;
         posY += offsetY;
         return new Phaser.Geom.Point(posX, posY);
